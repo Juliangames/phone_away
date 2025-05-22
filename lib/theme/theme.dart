ad102763c1841f719c4bef6a802d970f743a3b01
Radon-css <Julianschmuck@outlook.de>
2025-05-22 12:59:11 +0200

    Update theme.dart

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryColor = Color(0xFF066B5B);
+  static const Color primaryContainerColor = Color(0xFFA0F2DE);
   static const Color onPrimaryColor = Color(0xFFF5FBF7);
   static const Color surfaceColor = Color(0xFFF5FBF7);
   static const Color onSurfaceVariantColor = Color(0xFF3F4946);
