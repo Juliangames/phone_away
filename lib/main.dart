d13914baca614bb522b878462ceec1975ee6cf5f
Julian Ambrozy <ju351amb@htwg-konstanz.de>
2025-06-25 19:16:30 +0200

    hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My Flutter App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
