798b29eb81d2871418581ae4d9af2d5ecf58bb8f
HenrikFrs <141763686+HenrikFrs@users.noreply.github.com>
2025-07-30 21:22:54 +0200

    auth page tests

---
+++ test/mocks/firebase_auth_mock.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mockito/annotations.dart';
+
+// Erzeuge Mocks für FirebaseAuth und UserCredential
+@GenerateMocks([FirebaseAuth, UserCredential, User])
+void main() {}
